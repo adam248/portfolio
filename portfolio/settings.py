86d531f44515795075a5a39e563f8f5ae6a988e6
Adam Butler <adamjbutler091@gmail.com>
2023-01-25 10:23:13 +0930

    add project name

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+# Used for a default title
+APP_NAME = "Adam Butler's Portfolio"  # Add
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
